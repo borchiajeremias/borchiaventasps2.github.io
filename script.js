f3b646343934b581dd13302fe70e83eb53cf8527
borchiajeremias <borchiajeremias@gmail.com>
2026-07-03 22:24:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ loader.style.visibility="hidden";
 },2500);
 
 }
+const menu=document.querySelector(".hamburguesa");
+
+const nav=document.querySelector("nav");
+
+menu.onclick=()=>{
+
+nav.classList.toggle("active");
+
+}
