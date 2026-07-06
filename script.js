a2620f475f1b8350d619a50e5054f0e3c694c1f0
borchiajeremias <borchiajeremias@gmail.com>
2026-07-06 17:43:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -81,3 +81,107 @@ function actualizarContador() {
     contador.textContent = carrito.length;
 
 }
+const modal = document.getElementById("modal-carrito");
+
+const lista = document.getElementById("lista-carrito");
+
+const total = document.getElementById("total-productos");
+
+const cerrar = document.querySelector(".cerrar");
+
+const vaciar = document.getElementById("vaciar");
+
+const pagar = document.getElementById("pagar");
+
+const botonCarrito = document.querySelector(".cart");
+
+botonCarrito.addEventListener("click",()=>{
+
+    mostrarCarrito();
+
+    modal.style.display="flex";
+
+});
+
+cerrar.addEventListener("click",()=>{
+
+    modal.style.display="none";
+
+});
+
+function mostrarCarrito(){
+
+    lista.innerHTML="";
+
+    carrito.forEach((juego,index)=>{
+
+        lista.innerHTML+=`
+
+        <div class="item-carrito">
+
+            <span>${juego}</span>
+
+            <button onclick="eliminarProducto(${index})">
+
+                🗑
+
+            </button>
+
+        </div>
+
+        `;
+
+    });
+
+    total.innerText="Productos: "+carrito.length;
+
+}
+
+function eliminarProducto(i){
+
+    carrito.splice(i,1);
+
+    localStorage.setItem("carrito",JSON.stringify(carrito));
+
+    actualizarContador();
+
+    mostrarCarrito();
+
+}
+
+vaciar.addEventListener("click",()=>{
+
+    carrito=[];
+
+    localStorage.setItem("carrito","[]");
+
+    actualizarContador();
+
+    mostrarCarrito();
+
+});
+
+pagar.addEventListener("click",()=>{
+
+    if(carrito.length==0){
+
+        alert("El carrito está vacío.");
+
+        return;
+
+    }
+
+    alert("¡Gracias por tu compra!");
+
+    carrito=[];
+
+    localStorage.setItem("carrito","[]");
+
+    actualizarContador();
+
+    mostrarCarrito();
+
+    modal.style.display="none";
+
+});
+
